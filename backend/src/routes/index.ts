adf3bf96a704f9cb00a49113a1d46ec1a7e86a57
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-09 22:11:39 -0300

    feat(users): creating route and service for Users

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appontimentsRouter from './appointaments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appontimentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
