bbda134fd9fc56b24427a1f80917e683bff36b28
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-16 19:19:44 -0300

    feat(signup): creating tooltip for errors at Input during SignUp

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isError: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,16 +25,22 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.isError &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
-      color: #7d2929;
-      border-color: #7d2929;
+      color: #fff;
+      border-color: #fff;
     `}
 
   ${props =>
     props.isFilled &&
     css`
-      color: #7d2929;
+      color: #fff;
     `}
 
   input {
@@ -47,5 +56,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
