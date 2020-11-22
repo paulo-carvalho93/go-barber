87b34d003a8fe3e945c2b0197f9ab63128176d77
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-22 19:49:54 -0300

    feat(validation): including form validation with Yup lib

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 import {
   Image,
   View,
@@ -6,13 +6,16 @@ import {
   KeyboardAvoidingView,
   Platform,
   TextInput,
-  Text,
+  Alert,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -21,6 +24,12 @@ import logoImg from '../../assets/logo.png';
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  pasword: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
@@ -28,6 +37,46 @@ const SignUp: React.FC = () => {
   const passwordInputRef = useRef<TextInput>(null);
 
   const navigation = useNavigation();
+
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string()
+          .required('E-mail is required')
+          .email('Type a valid e-mail'),
+        password: Yup.string().required('Password is required'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // await api.post('/users', data);
+
+      // history.push('/');
+
+      Alert.alert(
+        'Successful registration!',
+        'You can now log on to GoBarber!',
+      );
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Registration error',
+        'An error occurred while registering, please try again!',
+      );
+    }
+  }, []);
 
   return (
     <>
@@ -47,12 +96,7 @@ const SignUp: React.FC = () => {
               <Title>Sign Up</Title>
             </View>
 
-            <Form
-              onSubmit={() => {
-                console.log('Aeee!');
-              }}
-              ref={formRef}
-            >
+            <Form onSubmit={handleSignUp} ref={formRef}>
               <Input
                 name="name"
                 icon="user"
