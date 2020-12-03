087fa1ad51d4a34ad7888edf753b9b72d03efa72
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-12-03 19:28:13 -0300

    refactor(tests): refactoring services tests

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -4,16 +4,19 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let createUser: CreateUserService;
+let fakeHashProvider: FakeHashProvider;
+
 describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+  });
+
   it('should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
     const user = await createUser.execute({
       name: 'Paulo Carvalho',
       email: 'paulo@example.com',
@@ -24,14 +27,6 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a new user with same email tha already exists', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
     await createUser.execute({
       name: 'Paulo Carvalho',
       email: 'paulo@example.com',
