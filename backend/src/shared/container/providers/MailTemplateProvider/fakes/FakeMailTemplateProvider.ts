e5676a10fb718582da140c8773a334e21b125c82
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-12-03 19:11:52 -0300

    feat(templateEngine): including templateEngine with a handlebars file HTML

--- backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import iMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements iMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
