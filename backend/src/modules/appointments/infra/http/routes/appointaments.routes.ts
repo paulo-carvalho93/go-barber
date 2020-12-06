d1ccbfdee214f614a333a953c4b49f64c6d1d153
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-12-06 20:26:39 -0300

    feat(provider): including route to provider see the appointments

--- backend/src/modules/appointments/infra/http/routes/appointaments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointaments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensuredAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensuredAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
