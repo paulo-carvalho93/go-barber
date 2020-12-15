281e972e7921dc69b598695b44ee8bfd23f12a59
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-12-15 18:53:31 -0300

    feat(dashboard): including styles for list of appointments

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Appointment,
+  Section,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,7 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
 
   return (
@@ -48,7 +51,7 @@ const Dashboard: React.FC = () => {
           </p>
 
           <NextAppointment>
-            <strong>Atendimento a seguir</strong>
+            <strong>Next appointments</strong>
             <div>
               <img
                 src="https://avatars2.githubusercontent.com/u/24879695?s=460&u=faf0d229fd01af1901c700a7dd07dfc19cf96180&v=4"
@@ -61,6 +64,73 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/24879695?s=460&u=faf0d229fd01af1901c700a7dd07dfc19cf96180&v=4"
+                  alt="Paulo"
+                />
+                <strong>Paulo Carvalho</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/24879695?s=460&u=faf0d229fd01af1901c700a7dd07dfc19cf96180&v=4"
+                  alt="Paulo"
+                />
+                <strong>Paulo Carvalho</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Evening</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/24879695?s=460&u=faf0d229fd01af1901c700a7dd07dfc19cf96180&v=4"
+                  alt="Paulo"
+                />
+                <strong>Paulo Carvalho</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/24879695?s=460&u=faf0d229fd01af1901c700a7dd07dfc19cf96180&v=4"
+                  alt="Paulo"
+                />
+                <strong>Paulo Carvalho</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
